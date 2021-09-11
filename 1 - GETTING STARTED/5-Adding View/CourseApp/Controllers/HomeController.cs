42f4e185fc3b00b3b89a9c4a33de91c3113e9122
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2021-09-11 18:40:26 +0300

    Adding View

---
+++ 1 - GETTING STARTED/5-Adding View/CourseApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApp.Controllers
+{
+    // localhost:5000
+    // localhost:5000/home
+
+    public class HomeController : Controller
+    {
+        // localhost:5000/home/index => home/index.cshtml
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // localhost:5000/home/about => home/about.cshtml
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
