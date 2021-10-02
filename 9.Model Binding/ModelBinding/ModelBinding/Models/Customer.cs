e0ae8f5e5e70bd86794bdaa0610c9af85f404455
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2021-10-03 00:08:03 +0300

    update

---
+++ 9.Model Binding/ModelBinding/ModelBinding/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBinding.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Address Address { get; set; }
+        public Role Role { get; set; }
+    }
+    public enum Role
+    {
+        Admin,
+        User,
+        Member
+    }
+}
---
+++ 9.Model Binding/ModelBinding/ModelBinding/Models/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModelBinding.Models
+{
+    public class Repository : IRepository
+    {
+       // private List<Customer> _customers;
+
+        public Repository()
+        {
+
+     
+        }
+
+        public List<Customer> Customers => throw new NotImplementedException();
+
+        public Customer Get(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
