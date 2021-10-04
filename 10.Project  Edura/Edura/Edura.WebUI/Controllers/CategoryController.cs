50b1ee87c32b747e6545e89152b4f43e741636c1
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2021-10-04 23:43:54 +0300

    updated

---
+++ 10.Project  Edura/Edura/Edura.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Edura.Repository.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Edura.WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ICategoryRepository repository;
+
+        public CategoryController(ICategoryRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public IActionResult Index()
+        {
+            var cat = repository.GetByName("Electronics");
+            return View(cat);
+        }
+    }
+}
