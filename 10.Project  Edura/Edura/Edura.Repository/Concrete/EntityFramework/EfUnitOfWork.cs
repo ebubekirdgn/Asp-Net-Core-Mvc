cc8643115ad75e46138a7caeb0f2dd53d7f11979
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2021-10-05 20:33:12 +0300

    UnitOfWork created

---
+++ 10.Project  Edura/Edura/Edura.Repository/Concrete/EntityFramework/EfUnitOfWork.cs
@@ -0,0 +1,64 @@
+﻿using Edura.Repository.Abstract;
+using Edura.Repository.Concrete.EntityFramework;
+using Edura.WebUI.Repository.Abstract;
+using System;
+
+namespace Edura.WebUI.Repository.Concrete.EntityFramework
+{
+    public class EfUnitOfWork : IUnitOfWork
+    {
+        private readonly EduraContext _dbContext;
+        public EfUnitOfWork(EduraContext dbContext)
+        {
+            //dbContext boş ise dedik '??' bunula
+            _dbContext = dbContext ?? throw new ArgumentException("dbcontext can not be null!");
+        }
+
+        public IProductRepository _products;
+
+        public ICategoryRepository _categories;
+
+        public IOrderRepository _orders;
+
+        public IProductRepository Products
+        {
+            get
+            {
+                return _products ?? (_products = new EfProductRepository(_dbContext));
+            }
+        }
+
+        public ICategoryRepository Categories
+        {
+            get
+            {
+                return _categories ?? (_categories = new EfCategoryRepository(_dbContext));
+            }
+        }
+
+        public IOrderRepository Orders
+        {
+            get
+            {
+                return _orders ?? (_orders = new EfOrderRepository(_dbContext));
+            }
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        public int SaveChanges()
+        {
+            try
+            {
+                return _dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
--- 10.Project  Edura/Edura/Edura.WebUI/Controllers/HomeController.cs
+++ 10.Project  Edura/Edura/Edura.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Edura.Repository.Abstract;
+﻿using Edura.Entity;
+using Edura.Repository.Abstract;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Edura.WebUI.Controllers
@@ -6,10 +7,12 @@ namespace Edura.WebUI.Controllers
     public class HomeController : Controller
     {
         private IProductRepository _repository;
+        private IUnitOfWork _unitOfWork;
 
-        public HomeController(IProductRepository repository)
+        public HomeController(IUnitOfWork unitOfWork, IProductRepository repository)
         {
             _repository = repository;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
@@ -20,6 +23,16 @@ namespace Edura.WebUI.Controllers
         public IActionResult Details(int id)
         {
             return View(_repository.Get(id));
+        }
+
+        public IActionResult Create ()
+        {
+            var product = new Product() { ProductName="Yeni Ürün" ,Price=1500};
+            _unitOfWork.Products.Add(product);
+            _unitOfWork.SaveChanges();
+
+            return RedirectToAction("Index");
+
         }
     }
 }
