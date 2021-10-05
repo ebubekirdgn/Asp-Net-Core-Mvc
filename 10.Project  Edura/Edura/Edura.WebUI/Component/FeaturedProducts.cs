a871ec375c770071f1cadc834301ae6f47de3bc1
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2021-10-06 00:56:10 +0300

    created FeaturedProducts

---
+++ 10.Project  Edura/Edura/Edura.WebUI/Component/FeaturedProducts.cs
@@ -0,0 +1,24 @@
+﻿using Edura.Repository.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Edura.WebUI.Components
+{
+    public class FeaturedProducts : ViewComponent
+    {
+        private IProductRepository repository;
+
+        public FeaturedProducts(IProductRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(repository
+                .GetAll()
+                .Where(x => x.IsApproved && x.IsFeatured)
+                .ToList());
+        }
+    }
+}
